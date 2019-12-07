58b5fb567bdbf315b497926ca1a86b57d13d99f7
Raulino Neto <raulino.neto@endurance.com>
2019-12-07 13:48:10 -0300

    adding cli

---
+++ internal/adapters/primary/cli.go
@@ -0,0 +1,47 @@
+package primary
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/lucasrosa/catvotes/internal/domains/votes"
+)
+
+type CLIPrimaryAdapter struct {
+	service votes.PrimaryPort
+}
+
+func NewCLIPrimaryAdapter(s votes.PrimaryPort) *APIGatewayPrimaryAdapter {
+	return &APIGatewayPrimaryAdapter{
+		s,
+	}
+}
+
+// PlaceOrder receives the request, processes it and returns a Response or an error
+func (a *CLIPrimaryAdapter) HandleVote() (string, error) {
+	var clientVote int
+	message := "Vote increased"
+	v := votes.Vote{}
+	v.Vote = true
+
+	flag.StringVar(&v.ImageID, "image", "", "image that will be voted")
+	flag.IntVar(&clientVote, "vote", 1, "increase vote to image")
+	flag.Parse()
+
+	if clientVote == 0 {
+		v.Vote = false
+		message = "Vote Decreased"
+	}
+
+	if !v.Vote && clientVote != 1 {
+		return "", fmt.Errorf("Vote only allows 1 or 0")
+	}
+
+	// Processing vote
+	err := a.service.Vote(v.ImageID, v.Vote)
+	if err != nil || clientVote > 1 {
+		return "", err
+	}
+
+	return message, nil
+}
